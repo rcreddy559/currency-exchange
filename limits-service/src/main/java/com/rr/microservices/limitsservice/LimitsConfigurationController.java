f2c3db24c656019f0aba416301e9004005517087
Ravichandra P <ravichandra.p@outlook.com>
2019-01-25 15:18:30 +0530

    newly Created

---
+++ limits-service/src/main/java/com/rr/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.rr.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rr.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfiguration() {
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+}
